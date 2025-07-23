1ff6254898063cbb3cdb408936fccf72ddcbe8ac
wemersonchaga <135094679+wemersonchaga@users.noreply.github.com>
2025-07-23 22:30:57 +0000

    adiciona gunicorn para deploy no Rende

--- djangoRestApp/tutorial/urls.py
+++ djangoRestApp/tutorial/urls.py
@@ -42,7 +42,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('nanny_pets_app.urls')),
-    path('', lambda request: redirect('/swagger/')),
+    path('swagger/api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
