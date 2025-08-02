797c56d65d73190853a6f4eeb1a735f100e3cfce
wemersonchaga <135094679+wemersonchaga@users.noreply.github.com>
2025-08-02 17:15:11 -0300

    Update urls.py
    organizando rota de imagem ambiente

--- djangoRestApp/nanny_pets_app/urls.py
+++ djangoRestApp/nanny_pets_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include, re_path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from django.shortcuts import redirect
 from .views import (
@@ -11,6 +11,7 @@ from .views import (
     TutorViewSet,
     PedidoViewSet,
     HospedagemViewSet,
+    UploadImagensAmbienteView,
     usuario_logado,
     PetViewSet,
     AvaliacaoCuidadorViewSet
@@ -24,15 +25,15 @@ router.register(r'hospedagens', HospedagemViewSet, basename='hospedagem')
 router.register(r'pets', PetViewSet, basename='pet')
 router.register(r'avaliacoes', AvaliacaoCuidadorViewSet, basename='avaliacoes')
 
-urlpatterns = router.urls
-
-urlpatterns += [
-    path('caracteristicas/', CaracteristicasAPIView.as_view(), name='caracteristicas-list'),
-    path('cuidadores/<int:cuidador_id>/caracteristicas/', CaracteristicasDoCuidadorView.as_view(), name='caracteristicas-cuidador'),
+urlpatterns = [
+    *router.urls,
+    
+    path('caracteristicas/', CaracteristicasAPIView.as_view(), name='listar_caracteristicas_gerais'),
+    path('cuidadores/<int:cuidador_id>/caracteristicas/', CaracteristicasDoCuidadorView.as_view(), name='caracteristicas_do_cuidador'),
+    path('cuidadores/imagens/upload/', UploadImagensAmbienteView.as_view(), name='upload_imagens_ambiente'),
     path('api/login/', CustomAuthToken.as_view(), name='api_login'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='api-login'),
     path('usuarios/me/', usuario_logado, name='usuario-logado'),
-    # Solução para erro 404 ao acessar /accounts/login/
     path('accounts/', include('django.contrib.auth.urls')),
 ]
